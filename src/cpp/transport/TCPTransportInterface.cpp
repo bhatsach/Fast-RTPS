9c0fba126008bd211ab9bae5c4cd80e5404a289c
LuisGasco <luisgasco@eprosima.com>
2019-02-22 12:14:37 +0100

    Refs #4672 WIP Debugging WAN scenario

--- src/cpp/transport/TCPTransportInterface.cpp
+++ src/cpp/transport/TCPTransportInterface.cpp
@@ -683,8 +683,8 @@ bool TCPTransportInterface::OpenOutputChannel(const Locator_t& locator)
     if (IsLocatorSupported(locator) && (logicalPort != 0))
     {
         std::unique_lock<std::mutex> scopedLock(sockets_map_mutex_);
-        logInfo(RTCP, "OpenOutputChannel (physical: " << IPLocator::getPhysicalPort(locator) << "; logical: "
-            << IPLocator::getLogicalPort(locator) << ") @ IP: " << IPLocator::toIPv4string(locator));
+        logError(RTCP, "Called to OpenOutputChannel (physical: " << IPLocator::getPhysicalPort(locator) << "; logical: "
+            << IPLocator::getLogicalPort(locator) << ") @ " << IPLocator::to_string(locator));
 
         const Locator_t& physicalLocator = IPLocator::toPhysicalLocator(locator);
         auto socketIt = channel_resources_.find(physicalLocator);
@@ -703,13 +703,14 @@ bool TCPTransportInterface::OpenOutputChannel(const Locator_t& locator)
         if (socketIt != channel_resources_.end())
         {
             channel = socketIt->second;
+            logError(OpenOutputChannel, "OpenOutputChannel already opened");
         }
         else
         {
             // Create output channel
             logError(OpenOutputChannel, "OpenOutputChannel (physical: "
                 << IPLocator::getPhysicalPort(locator) << "; logical: "
-                << IPLocator::getLogicalPort(locator) << ") @ IP: " << IPLocator::toIPv4string(locator));
+                << IPLocator::getLogicalPort(locator) << ") @ " << IPLocator::to_string(locator));
 #if TLS_FOUND
             if (configuration()->apply_security)
             {
